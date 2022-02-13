26e430fb8593950ea12e56408a1bd36c89aaa8d0
lorraine98 <hyosung0425@naver.com>
2022-02-14 03:56:04 +0900

    Add useful matchers

--- fn.js
+++ fn.js
@@ -1,4 +1,8 @@
 const fn = {
-    add: (num1, num2) => num1 + num2
-}
+    add: (num1, num2) => num1 + num2,
+    makeUser: (name, age) => ({ name, age, gender: undefined }),
+    throwErr: () => {
+        throw new Error('error');
+    }
+};
 module.exports = fn;
